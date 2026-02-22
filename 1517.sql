cb442e4553293de8a3bf10296901ce33335a28a0
Saniya Khan <saniyakhan1709@gmail.com>
2026-02-22 22:55:28 +0530

    Create 1517
    Query to find users with valid LeetCode emails.

---
+++ 1517.sql
@@ -0,0 +1,11 @@
+-- 1517. Find Users With Valid E-Mails
+
+SELECT user_id, name , mail 
+FROM Users
+WHERE mail REGEXP '^[a-zA-Z][a-zA-Z0-9_.\\-]*@leetcode\\.com$'
+AND mail LIKE BINARY '%@leetcode.com'
+;
+
+
+
+-- this was tiring.
